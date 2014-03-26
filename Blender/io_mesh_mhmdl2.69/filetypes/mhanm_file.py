878aab3147160ba1c76bdf6e60c05c9aba45cb70
WorldSEnder <worldsbegin@gmx.de>
2014-03-26 15:37:01 +0100

    Introduced blender version control to export-script-location

--- Blender/io_mesh_mhmdl2.69/filetypes/mhanm_file.py
+++ Blender/io_mesh_mhmdl2.69/filetypes/mhanm_file.py
@@ -1,7 +1,8 @@
+from aaaa import brea
 
 class MhAnm:
     def __init__(self, resLocation):
         self.resLocation = resLocation
         
     def export_to_file(self, pathDir):
-        pass
+        pass # TODO: write an animation exporter
